3650ed032c3e78867f76cd8a874db648f7c5c975
Kyra Hamerling-Potts <khamerling-potts@wustl.edu>
2024-01-04 11:18:38 -0500

    done

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -1,40 +1,127 @@
+# National parks            Visitors
+#                   Trips
+
+
 class NationalPark:
+    all = []
 
     def __init__(self, name):
         self.name = name
-        
+        type(self).all.append(self)
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, name):
+        if not hasattr(self, "name") and isinstance(name, str) and len(name) >= 3:
+            self._name = name
+        else:
+            raise Exception("national park name must be a str more than 3 chars long")
+
     def trips(self):
-        pass
-    
+        return [trip for trip in Trip.all if trip.national_park == self]
+
     def visitors(self):
-        pass
-    
+        return list({trip.visitor for trip in self.trips()})
+
     def total_visits(self):
-        pass
-    
+        return len(self.trips())
+
     def best_visitor(self):
-        pass
+        visitors = [trip.visitor for trip in self.trips()]
+        return max(visitors, key=visitors.count)
+        # for trip in self.trips():
+        #     if trip.visitor in visitors:
+        #         visitors[trip.visitor] += 1
+        #     else:
+        #         visitors[trip.visitor] = 0
+        # return max(visitors, key=lambda visitor: visitors[visitor])
+        # # or use key=visitors.get
+        # # for each visitor, find the value of visitors[visitor] and use max of that
+        # # visitor => visitors.get(visitor)
 
 
 class Trip:
-    
+    all = []
+
     def __init__(self, visitor, national_park, start_date, end_date):
         self.visitor = visitor
         self.national_park = national_park
         self.start_date = start_date
         self.end_date = end_date
+        type(self).all.append(self)
+
+    @property
+    def start_date(self):
+        return self._start_date
+
+    @start_date.setter
+    def start_date(self, start_date):
+        if isinstance(start_date, str) and len(start_date) >= 7:
+            self._start_date = start_date
+        else:
+            raise Exception("start date must be a string at least 7 chars long")
+
+    @property
+    def end_date(self):
+        return self._end_date
+
+    @end_date.setter
+    def end_date(self, end_date):
+        if isinstance(end_date, str) and len(end_date) >= 7:
+            self._end_date = end_date
+        else:
+            raise Exception("end date must be a string at least 7 chars long")
+
+    @property
+    def visitor(self):
+        return self._visitor
+
+    @visitor.setter
+    def visitor(self, visitor):
+        if isinstance(visitor, Visitor):
+            self._visitor = visitor
+        else:
+            raise Exception("visitor must be a Visitor")
+
+    @property
+    def national_park(self):
+        return self._national_park
+
+    @national_park.setter
+    def national_park(self, national_park):
+        if isinstance(national_park, NationalPark):
+            self._national_park = national_park
+        else:
+            raise Exception("national park must be a NationalPark")
 
 
 class Visitor:
+    all = []
 
     def __init__(self, name):
         self.name = name
-        
+        type(self).all.append(self)
+
+    # getter
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, name):
+        if isinstance(name, str) and 1 <= len(name) <= 15:
+            self._name = name
+        else:
+            raise Exception("visitor name must be a string between 1-15 chars long")
+
     def trips(self):
-        pass
-    
+        return [trip for trip in Trip.all if trip.visitor == self]
+
     def national_parks(self):
-        pass
-    
+        return list({trip.national_park for trip in self.trips()})
+
     def total_visits_at_park(self, park):
-        pass
+        return len([trip for trip in self.trips() if trip.national_park == park])
--- lib/debug.py
+++ lib/debug.py
@@ -5,7 +5,18 @@ from classes.many_to_many import NationalPark
 from classes.many_to_many import Visitor
 from classes.many_to_many import Trip
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     print("HELLO! :) let's debug :vibing_potato:")
+    breakpoint()
+    vis_1 = Visitor("Tammothy")
+    vis_2 = Visitor("Bryce")
+    vis_3 = Visitor("Wolfe")
+    p_1 = NationalPark("Alaska Wilds")
 
+    Trip(vis_1, p_1, "February 2nd", "February 3rd")
+    Trip(vis_2, p_1, "February 5th", "February 9th")
+
+    assert vis_1 in p_1.visitors()
+    assert vis_2 in p_1.visitors()
+    assert vis_3 not in p_1.visitors()
     ipdb.set_trace()
